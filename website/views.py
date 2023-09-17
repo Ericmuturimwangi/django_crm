b8e5a0612fa585cd6dc10aeb81e5030f481da1df
Ericmuturimwangi <muturiwamwanginanyawira@gmail.com>
2023-09-17 00:23:17 -0400

    customer relationship manager

--- website/views.py
+++ website/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
-
-
+from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 
-
 def home(request):
-    #check to see if loggin in
-
+    records = Record.objects.all()
+    
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -24,7 +22,7 @@ def home(request):
             messages.error(request, "There was an error")  # Changed from "success" to "error"
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 def logout_user(request):
     logout (request)
@@ -32,6 +30,29 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #auth and login
+            username= form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            user= authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have SUccessfully Registered")
+            return redirect('home')
 
+    else:
+        form=SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
 
+def customer_record (request, pk):
+    if request.user.is_authenticated:
+        #look up record
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.error(request, "YOu must be logged in to view that") 
+        return redirect('home')
